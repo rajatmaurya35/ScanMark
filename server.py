d59b904e434fe65f27be5be518858f2c99f2f6cb
rajatmaurya35 <rajatmaurya35@gmail.com>
2025-04-11 15:47:45 +0530

    Fix Method Not Allowed and Google OAuth errors

--- server.py
+++ server.py
@@ -3,8 +3,12 @@ from app import app as main_app
 
 app = Flask(__name__)
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
+@app.route('/', defaults={'path': ''}, methods=['GET', 'POST'])
+@app.route('/<path:path>', methods=['GET', 'POST'])
 def catch_all(path):
     with main_app.test_client() as client:
+        if request.method == 'POST':
+            return client.post('/' + path, 
+                             data=request.form,
+                             headers=request.headers).get_data(as_text=True)
         return client.get('/' + path).get_data(as_text=True)
